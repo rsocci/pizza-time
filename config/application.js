7a1188b0869414b6c1789b27432bd4753af88230
Lin Reid <linreid@gmail.com>
2013-12-09 12:07:21 -0500

    Initial Commit

---
+++ config/application.js
@@ -0,0 +1,12 @@
+//= require jquery
+//= require environment
+//= require ember-appkit
+//= require ember-data
+//= require_self
+//= require adapter
+//= require router
+//= require jquery.ui.all
+//= require_tree ../app
+//= require_tree ./initializers
+
+window.App = require('app').default.create();
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PizzaTime::Application.config.secret_key_base = '05fb901fa47789f2ae30b386ae33f16cf548c3238aa613c48abe71b1973dc003df9e77863a62055200ff3e2265b2226b63e8841c7944ac0c3c642ef3e7231984'
