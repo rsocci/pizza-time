4f9eaf546e2d50019b2177d3488bfe34a3801783
Lin Reid <linreid@gmail.com>
2014-02-01 11:59:57 -0500

    Add city field to meetup search
    * Adds 'city' field to meetup search so api search does not default to
     server location.
    * Adds ember-easyForm
    * Adds ember-validations
    * Modifies GroupsController#index to take a location parameter
    
    * Update eak-rails to latest version
    * Adds error message if config/secrets.yml is not present

--- config/initializers/load_secrets.rb
+++ config/initializers/load_secrets.rb
@@ -1,7 +1,15 @@
 PizzaTime::Application.configure do
   # Note: Hashie is a gem dependency
   if ENV['RAILS_ENV'] == 'development'
-    config.secrets = Hashie::Mash.new(YAML.load_file(Rails.root.join('config', 'secrets.yml'))[Rails.env.to_s])
+    secrets_file = Rails.root.join('config', 'secrets.yml')
+
+    config.secrets = if File.exist?(secrets_file)
+      Hashie::Mash.new(YAML.load_file(secrets_file)[Rails.env.to_s])
+    else
+      $stderr.puts "We were unable to locate the required API keys. Please enter them in `config/secrets.yml`."
+      Hashie::Mash.new
+    end
+
     ENV['meetup_key'] = config.secrets.meetup_key
   end
 end
